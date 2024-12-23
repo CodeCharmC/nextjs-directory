a4f529bcd31da30e3f7944f3898f5c1de53247e0
CodeCharmC <moni.akhtar1066@gmail.com>
2024-12-23 22:34:02 +0600

    create routes with next js file route system

---
+++ app/components/hello.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export default function Hello() {
+   return <>
+      <h1 className="text-3xl font-bold text-orange-300">
+         Hello
+      </h1>
+   </>
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+
+export default function About() {
+   return (
+      <>
+         <h1 className="text-3xl font-bold  flex justify-center pt-3">
+            Dashboard 
+         </h1>
+      </>
+   );
+}
