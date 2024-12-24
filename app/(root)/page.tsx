c6a12eb149c9239308095bcbd1136491b49b426b
CodeCharmC <moni.akhtar1066@gmail.com>
2024-12-24 23:46:22 +0600

    create group root and transfer about folder and home page in it

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,11 @@
-import Hello from "./components/hello";
+import Hello from "../components/hello";
+
 
 export default function Home() {
   return (
     <>
       <h1>Welcome to Next.js app-3 @.@</h1>
-      <Hello />
+      <Hello/>
     </>
   );
 }
