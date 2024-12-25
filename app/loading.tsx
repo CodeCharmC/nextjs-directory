577c5c6a806de6e5e3328ebc58bac8a36b315940
CodeCharmC <moni.akhtar1066@gmail.com>
2024-12-25 17:25:14 +0600

    create error specialized component in root dir

---
+++ app/loading.tsx
@@ -0,0 +1,12 @@
+
+
+import React from 'react'
+
+export default function Loading() {
+   console.log('loading...')
+   return (
+      <>
+         <div>Loading...     -__________-</div>
+      </>
+   )
+}
