7e0e8b88e8f94b9264c3933cc23da3094900d290
CodeCharmC <moni.akhtar1066@gmail.com>
2024-12-25 09:03:42 +0600

    practic more with (root)

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,8 +5,9 @@ import React from 'react'
 export default function layout({children}: {children: React.ReactNode}) {
    return (
       <>
-         <h1 className='text-3xl font-bold pt-3'>Root layout</h1>
+         <h1 className='text-3xl font-bold pt-3'>NAVBAR</h1>
          {children}
+         <h1 className='text-3xl font-bold bottom-0'>FOOTER</h1>
       </>
    )
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,7 +4,7 @@ import Hello from "../components/hello";
 export default function Home() {
   return (
     <>
-      <h1>Welcome to Next.js app-3 @.@</h1>
+      <h1 className="text-3xl font-bold">Welcome to Next.js app-3 @.@</h1>
       <Hello/>
     </>
   );
