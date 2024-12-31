90afab2af8c2735c36bce2eb336eb45f6c7d2ab8
CodeCharmC <moni.akhtar1066@gmail.com>
2024-12-31 23:26:01 +0600

    add logo in navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,20 @@
+import Link from "next/link"
+import Image from "next/image"
 
 export default function Navbar() {
    return (
-      <div>Navbar</div>
+      <header className="px-5 py-4 bg-red-500 shadow-sm font-sans">
+         <nav className="flex items-center justify-between">
+            <Link href="/">
+               <Image
+                  src="/directory.png"
+                  alt="Logo"
+                  width={80}
+                  height={80}
+                  priority
+               />
+            </Link>
+         </nav>
+      </header>
    )
 }
