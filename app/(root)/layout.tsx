833cb57891019ef00fdb771f3dd8f68f2550f755
CodeCharmC <moni.akhtar1066@gmail.com>
2025-01-02 23:31:25 +0600

    convert it for root group

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,16 +1,12 @@
 import Navbar from "@/app/components/Navbar";
 
-export default function layout(
+export default function Layout(
    { children }: Readonly<{ children: React.ReactNode }>  
 ) {
    return (
-      <html>
-         <body>
-            <main className="font-work-sans">
-               <Navbar />
-               {children}
-            </main>
-         </body>
-      </html>
+      <main>
+         <Navbar />
+         {children}
+      </main>
    )
 }
