0e3f9fd7cdf0258ef61078432cb8cd41e69eb006
CodeCharmC <moni.akhtar1066@gmail.com>
2025-01-06 00:00:05 +0600

    add smome demo card

--- components/EventsCard.tsx
+++ components/EventsCard.tsx
@@ -1,6 +1,40 @@
 
 export default function EventsCard() {
    return (
-     <></> 
+      <>
+      <div className="p-4 m-4 border-gray-800 border-[1px] max-w-[350px] min-h-[400px] rounded-md">
+         <p>Events Card</p>
+      </div>
+      <div className="p-4 m-4 border-gray-800 border-[1px] max-w-[300px] min-h-[400px] rounded-md">
+         <p>Events Card</p>
+      </div>
+      <div className="p-4 m-4 border-gray-800 border-[1px] max-w-[300px] min-h-[400px] rounded-md">
+         <p>Events Card</p>
+      </div>
+      <div className="p-4 m-4 border-gray-800 border-[1px] max-w-[300px] min-h-[400px] rounded-md">
+         <p>Events Card</p>
+      </div>
+      <div className="p-4 m-4 border-gray-800 border-[1px] max-w-[300px] min-h-[400px] rounded-md">
+         <p>Events Card</p>
+      </div>
+      <div className="p-4 m-4 border-gray-800 border-[1px] max-w-[300px] min-h-[400px] rounded-md">
+         <p>Events Card</p>
+      </div>
+      <div className="p-4 m-4 border-gray-800 border-[1px] max-w-[300px] min-h-[400px] rounded-md">
+         <p>Events Card</p>
+      </div>
+      <div className="p-4 m-4 border-gray-800 border-[1px] max-w-[300px] min-h-[400px] rounded-md">
+         <p>Events Card</p>
+      </div>
+      <div className="p-4 m-4 border-gray-800 border-[1px] max-w-[300px] min-h-[400px] rounded-md">
+         <p>Events Card</p>
+      </div>
+      <div className="p-4 m-4 border-gray-800 border-[1px] max-w-[300px] min-h-[400px] rounded-md">
+         <p>Events Card</p>
+      </div>
+      <div className="p-4 m-4 border-gray-800 border-[1px] max-w-[300px] min-h-[400px] rounded-md">
+         <p>Events Card</p>
+      </div>
+   </> 
    )
 }
