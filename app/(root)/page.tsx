d4e94ef2bd3f5cfa33a2d43a35bf6f803ca6c00d
CodeCharmC <moni.akhtar1066@gmail.com>
2025-01-06 15:51:11 +0600

    complate home ui deisgn

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,18 +1,94 @@
 import EventsCard from "@/components/EventsCard";
 import SearchForm from "../../components/SearchForm";
 
-export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }>}) {
+const Post = [
+   {
+      _createdAt: "2023-05-01",
+      _id: 1,
+      views: 59,
+      author: {_id:1, name:"Devid Melen"},
+      title: "Codeforces Round 1",
+      description: "This is a description. This is a description.This is a description.This is a description.This is a description.This is a description.This is a description.This is a description.This is a description.This is a description.This is a description.",
+      category: "Codeforces",
+      image:"https://cdn.pixabay.com/photo/2024/01/25/05/47/ai-generated-8531013_1280.jpg"
+   },
+   {
+      _createdAt: "2023-05-01",
+      _id: 1,
+      views: 59,
+      author: {_id:1, name:"Devid Melen"},
+      title: "Codeforces Round 1",
+      description: "This is a description. This is a description.This is a description.This is a description.This is a description.This is a description.This is a description.This is a description.This is a description.This is a description.This is a description.",
+      category: "Codeforces",
+      image:"https://cdn.pixabay.com/photo/2022/04/01/09/08/particles-7104407_1280.jpg"
+   },
+   {
+      _createdAt: "2023-05-01",
+      _id: 1,
+      views: 59,
+      author: {_id:1, name:"Devid Melen"},
+      title: "Codeforces Round 1",
+      description: "This is a description. This is a description.This is a description.This is a description.This is a description.This is a description.This is a description.This is a description.This is a description.This is a description.This is a description.",
+      category: "Codeforces",
+      image:"https://cdn.pixabay.com/photo/2016/11/17/23/29/programming-1832991_1280.png"
+   },
+   {
+      _createdAt: "2023-05-01",
+      _id: 1,
+      views: 59,
+      author: {_id:1, name:"Devid Melen"},
+      title: "Codeforces Round 1",
+      description: "This is a description. This is a description.This is a description.This is a description.This is a description.This is a description.This is a description.This is a description.This is a description.This is a description.This is a description.",
+      category: "Codeforces",
+      image:"https://cdn.pixabay.com/photo/2014/12/30/13/44/programming-583923_1280.jpg"
+   },
+   
+   {
+      _createdAt: "2023-05-01",
+      _id: 1,
+      views: 59,
+      author: {_id:1, name:"Devid Melen"},
+      title: "Codeforces Round 1",
+      description: "This is a description. This is a description.This is a description.This is a description.This is a description.This is a description.This is a description.This is a description.This is a description.This is a description.This is a description.",
+      category: "Codeforces",
+      image:"https://cdn.pixabay.com/photo/2021/11/12/06/54/globe-6788156_1280.jpg"
+   },
+   {
+      _createdAt: "2023-05-01",
+      _id: 1,
+      views: 59,
+      author: {_id:1, name:"Devid Melen"},
+      title: "Codeforces Round 1",
+      description: "This is a description. This is a description.This is a description.This is a description.This is a description.This is a description.This is a description.This is a description.This is a description.This is a description.This is a description.",
+      category: "Codeforces",
+      image:"https://cdn.pixabay.com/photo/2021/09/09/04/38/binary-6609473_1280.jpg"
+   },
+   {
+      _createdAt: "2023-05-01",
+      _id: 1,
+      views: 59,
+      author: {_id:1, name:"Devid Melen"},
+      title: "Codeforces Round 1",
+      description: "This is a description. This is a description.This is a description.This is a description.This is a description.This is a description.This is a description.This is a description.This is a description.This is a description.This is a description.",
+      category: "Codeforces",
+      image:"https://cdn.pixabay.com/photo/2016/11/17/23/29/programming-1832991_1280.png"
+   },
+]
+
+export default async function Home({ searchParams }: {
+   searchParams: Promise<{ query?: string }>
+}) {
    const query = (await searchParams).query;
    return (
       <>
-         <section className="dot_container w-full text-left flex-center">
-            <div className="max-width">
-               <h3 className="text-bold mb-4">
+         <section className="dot_container w-full">
+            <div className="hero-container gap-4 sm:gap-6">
+               <h3 className="text-bold">
                   <span className="pl-2">
                      Solve, Compete and Win
                   </span>
                </h3>
-               <h1 className="text-extrabold mb-6">Crack Complex Problems, Showcase Algorithmic Brilliance and Build Connections</h1>
+               <h1 className="text-extrabold">Crack Complex Problems, Showcase Algorithmic Brilliance and Build Connections</h1>               
                <p className="text-normal">Participate in Challenging Events, Master Algorithmic Logic, and Dominate the Competitive Programming Arena.</p>
                <SearchForm query={query} />               
             </div> 
@@ -23,9 +99,15 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ q
                   {query ? `${query}`: "All Events"}
                </p>               
             </div>            
-            <div className="w-full border-gray-800 border-[1px] grid grid-cols-1 sm:grid-cols-2  md:grid-cols-3 lg:grid-cols-4 gap-2 p-4 ">
-               <EventsCard />               
-            </div>
+            <ul className="cards-container">
+               {Post?.length > 0 ? (
+                  Post.map((post: any) => (
+                     <EventsCard key={post?._id} post={post} />
+                  ))
+               ) : (
+                     <p className="text-center">No events found</p>
+               )}                              
+            </ul>
          </section>
       </>
    )
