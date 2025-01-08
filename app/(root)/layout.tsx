7e61f781c499acfc93d57d0d729ea1c444f73a8d
CodeCharmC <moni.akhtar1066@gmail.com>
2025-01-08 21:15:55 +0600

    add home page and layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,14 @@
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 
 export default function Layout(
    { children }: { children: React.ReactNode }
 ) {
    return (
       <main>
+         <Navbar />         
          {children}
+         <Footer />
       </main>
    )
 }
