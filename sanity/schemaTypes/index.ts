9ab4f3cce05c512bbebc4d2e0ce7ff5df30ee976
CodeCharmC <moni.akhtar1066@gmail.com>
2025-01-09 22:09:29 +0600

    add events to index

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,6 +1,7 @@
 import { type SchemaTypeDefinition } from 'sanity'
 import { author } from './author'
+import { events } from './events'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [ author],
+  types: [ author, events],
 }
