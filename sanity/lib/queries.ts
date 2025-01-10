631bf5333a584161a69b532afebb9aad2813778f
CodeCharmC <moni.akhtar1066@gmail.com>
2025-01-10 12:24:46 +0600

    create events queries for sanity so the post can be fetch form sanity

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,7 +1,7 @@
 import { defineQuery } from "next-sanity";
 
 export const EVENTS_QUERY =
-   defineQuery(`*[_type == "events" && defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search] | order(_createdAt desc) {
+   defineQuery(`*[_type == "events" && defined(slug.current) ] {
    _id, 
    title, 
    slug,
