2319ae9c7b959a3e6dd1b1b905bd2404cae13acd
CodeCharmC <moni.akhtar1066@gmail.com>
2025-01-11 23:45:54 +0600

    create Post by fetching query by id

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,11 +1,16 @@
+import { client } from "@/sanity/lib/client"
+import { EVENTS_BY_ID_QUERY } from "@/sanity/lib/queries"
+
 
 export default async function Page(
    { params }: { params: Promise<{ id: string }> }   
 ) {
    const id = (await params).id
+   const { data: Post } = await client.fetch(EVENTS_BY_ID_QUERY, { id })   
+   if (!Post) return null
    return (
       <>
-         page no {id}
+         page no {Post }         
       </>
    )
 }
