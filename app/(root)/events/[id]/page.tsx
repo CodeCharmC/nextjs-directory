36d20dcfe5c257a263b2c22ef96101183ffede38
CodeCharmC <moni.akhtar1066@gmail.com>
2025-01-12 22:17:49 +0600

    update the hero part

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -14,52 +14,17 @@ export default async function Page(
    if (!post) return notFound()   
    return (
       <>
-         <section className="dot_container !min-h-[230px] gap-6">
-            <p className="text-bold">(post?.createdAt)</p>
-            <h1 className="text-extrabold !text-left">{post.title}</h1>
-            <p className="text-normal !text-gray-200 !max-w-5xl">{post.description}</p>
-         </section>
-
-         <section className="section_container">
-            <img
-               src={post.image}
-               alt="thumbnail"
-               className="w-full h-auto rounded-xl"
-            />
-
-            <div className="space-y-5 mt-10 max-w-4xl mx-auto">
-               <div className="flex-between gap-5">
-                  <Link
-                     href={`/user/${post.author?._id}`}
-                     className="flex gap-2 items-center mb-3"
-                  >
-                     <Image
-                        src={post.author.image}
-                        alt="avatar"
-                        width={64}
-                        height={64}
-                        className="rounded-full drop-shadow-lg"
-                     />
-                     <div>
-                        <p className="text-20-medium">{post.author.name}</p>
-                        <p className="text-16-medium !text-black-300">
-                           @{post.author.username}
-                        </p>
-                     </div>
-                  </Link>
-                  <p className="category-tag">{post.category}</p>
-               </div>
-               <h3 className="text-30-bold">Pitch Details</h3>
-               
-
-
-            </div>
-            <hr className="divider" />
-            
-
-
-         </section> 
-         <h1 className="flex-center ">page no {post?.title}</h1>                  
+         <section className="!min-h-[230px]  dot_container w-full">
+            <div className="hero-container gap-4 sm:gap-6">
+               <h3 className="text-bold">
+                  <span className="pl-2">
+                  (post?.createdAt)
+                  </span>
+               </h3>
+               <h1 className="text-extrabold">{post.title}</h1>               
+               <p className="text-normal">{post.description}</p>               
+            </div> 
+         </section>         
       </>
    )
 }
