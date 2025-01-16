6c35306d9af4e792d8d0317501f8a2b302bddc8d
CodeCharmC <moni.akhtar1066@gmail.com>
2025-01-16 14:09:47 +0600

    create Views component

---
+++ components/Views.tsx
@@ -0,0 +1,25 @@
+import { client } from "@/sanity/lib/client";
+import Dot from "./Dot";
+import { EVENTS_VIEWS_QUERY } from "@/sanity/lib/queries";
+import { formatViews } from "@/lib/utils";
+
+export default async function Views(
+   { id }: { id: string }
+) {
+   const { views: totalViews } = await client.withConfig({ useCdn: false }).fetch(EVENTS_VIEWS_QUERY, { id });
+   
+   //todo
+
+
+   return (
+      <div className="flex justify-end items-center mt-5 fixed bottom-3 right-3">
+         <div className="absolute -top-2 -right-2 ">
+            <Dot/>
+         </div>
+         <p className="font-medium text-[16px] bg-gray-950 px-4 py-2 rounded-lg capitalize">
+            <span> {formatViews(totalViews)}</span>
+         </p>
+
+      </div>
+   )
+}
