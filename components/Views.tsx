8d3899beaa9ac3e0a2fcaf86b66779512645147c
CodeCharmC <moni.akhtar1066@gmail.com>
2025-01-16 21:45:12 +0600

    update views with each visit

--- components/Views.tsx
+++ components/Views.tsx
@@ -2,15 +2,18 @@ import { client } from "@/sanity/lib/client";
 import Dot from "./Dot";
 import { EVENTS_VIEWS_QUERY } from "@/sanity/lib/queries";
 import { formatViews } from "@/lib/utils";
+import { writeClient } from "@/sanity/lib/write-client";
 
 export default async function Views(
    { id }: { id: string }
 ) {
    const { views: totalViews } = await client.withConfig({ useCdn: false }).fetch(EVENTS_VIEWS_QUERY, { id });
+
+   await writeClient
+      .patch(id)
+      .set({ views: totalViews + 1 })
+      .commit();
    
-   //todo
-
-
    return (
       <div className="flex justify-end items-center mt-5 fixed bottom-3 right-3">
          <div className="absolute -top-2 -right-2 ">
