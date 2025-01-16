eea926892315314099a358f9e13fe4d0464db713
CodeCharmC <moni.akhtar1066@gmail.com>
2025-01-16 14:07:52 +0600

    the page of [id] s

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -3,8 +3,16 @@ import { EVENTS_BY_ID_QUERY } from "@/sanity/lib/queries"
 import { notFound } from "next/navigation"
 import Image from "next/image"
 import Link from "next/link"
+import { HashIcon } from "lucide-react"
+import { formatDate } from "@/lib/utils"
+import markdownit from "markdown-it"
+import { Suspense } from "react"
+import { Skeleton } from "@/components/ui/skeleton"
+import Views from "@/components/Views"
 
 export const experimental_ppr = true
+
+const md = markdownit()
 
 export default async function Page(
    { params }: { params: Promise<{ id: string }> }   
@@ -12,13 +20,14 @@ export default async function Page(
    const id = (await params).id
    const  post  = await client.fetch(EVENTS_BY_ID_QUERY, { id })    
    if (!post) return notFound()   
+   const parsedContent = md.render(post?.pitch || "");
    return (
       <>
          <section className="!min-h-[230px]  dot_container w-full">
             <div className="hero-container gap-4 sm:gap-6">
                <h3 className="text-bold">
                   <span className="pl-2">
-                  (post?.createdAt)
+                  {formatDate(post._createdAt)}
                   </span>
                </h3>
                <h1 className="text-extrabold">{post.title}</h1>               
@@ -26,14 +35,57 @@ export default async function Page(
             </div> 
          </section> 
          <section className="flex-center flex-col gap-4 mt-8 mb-10 max-width">
-            <div className="max-w-[900px] min-w-[180px] max-h-[500px] min-h-[150px] flex-center ml-6 mr-6">
+
+            <div className="w-full flex-center m-15 bg-gray-950 max-w-6xl">
                <img
                   src={post.image}
                   alt="thumbnail"
                   className="w-full h-full rounded-xl"
                />
             </div>
-         </section>
+
+            <div className="space-y-5 mt-10 max-w-6xl mx-auto flex flex-col gap-4 w-full">
+               <div className="w-full flex-between flex-row">
+                  <Link
+                     href={`/user/${post.author?._id}`}
+                     className="flex gap-2 items-center mb-3"
+                  >
+                     <Image
+                        src={post.author.image}
+                        alt="avatar"
+                        width={64}
+                        height={64}
+                        className="w-[50px] h-[50px] rounded-full drop-shadow-lg" 
+                     />
+                     <div>
+                        <p className="text-xl font-semibold">{post.author.name}</p>
+                        <p className="text-lg font-semibold">
+                           @{post.author.username}
+                        </p>
+                     </div>
+                  </Link>
+                  <p className="card-normaltext flex-center flex-row">
+                     <HashIcon width={12} className="text-gray-200" />
+                     <span>{post.category}</span>
+                  </p> 
+               </div>
+               <h3 className="max-w-[280px] md:max-w-[300px] text-base md:text-lg font-bold">Pitch Details</h3>
+               {parsedContent ? (
+                  <article
+                     dangerouslySetInnerHTML={{__html: parsedContent}}
+                  />
+               ) : (
+                     <p>No details provided</p>
+               )}
+
+            </div>
+            
+            <hr className="border-dotted bg-gray-400 max-w-4xl my-10 mx-auto" /> 
+            
+            <Suspense fallback={<Skeleton className="flex justify-end items-center mt-5 fixed bottom-3 right-3 " />}>
+               <Views id={id} />
+            </Suspense>
+         </section>           
       </>
    )
 }
