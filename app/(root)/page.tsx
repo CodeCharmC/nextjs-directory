3c2ea05a761293633ceae257c52350e096e01d7d
CodeCharmC <moni.akhtar1066@gmail.com>
2025-01-17 23:30:12 +0600

    try out the session for id

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -11,7 +11,7 @@ export default async function Home({ searchParams }: {
    const params = { search: query || null }
    
    const session = await auth();
-   console.log(session);
+   console.log(session?.id);
 
    const {data: Post } = await sanityFetch({ query: EVENTS_QUERY, params });
    return (
