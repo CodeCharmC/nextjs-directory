519d047b8c4f2027492ee04d09180c8f51ddea3f
CodeCharmC <moni.akhtar1066@gmail.com>
2025-02-18 22:36:58 +0600

    create a normal form with a input feild

--- components/EventForm.tsx
+++ components/EventForm.tsx
@@ -1,6 +1,27 @@
 "use client"
+
+import { Input } from "@/components/ui/input"
+
 export default function EventForm() {
   return (
-      <div>EventForm</div>
+    <>
+      <form
+        // action={formAction} 
+        className="events-form">
+        <div>
+          <label htmlFor="title" className="events-form_label">
+            Title
+          </label>
+          <Input
+            id="title"
+            name="title"
+            className="events-form_input"
+            required
+            placeholder="events Title"
+          />
+          
+        </div>
+      </form>
+    </>
   )
 }
